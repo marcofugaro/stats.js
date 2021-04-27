d80669816fd0bd6bbc3a6aaf94b3d28ba6333afa
Marco Fugaro <marco.fugaro@gmail.com>
2021-04-27 22:14:59 +0200

    Rollup: use terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
 	input: 'src/Stats.js',
 	output: [
 		{
 			format: 'umd',
+			exports: 'named',
 			name: 'Stats',
 			file: 'build/stats.js',
 			indent: '\t'
 		},
 		{
-			format: 'es',
+			format: 'umd',
+			exports: 'named',
 			name: 'Stats',
+			plugins: [
+				terser(),
+			],
+			file: 'build/stats.min.js',
+		},
+		{
+			format: 'esm',
+			exports: 'named',
 			file: 'build/stats.module.js',
-			indent: '\t'
 		}
 	]
 };
