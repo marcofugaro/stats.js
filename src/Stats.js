fbb0852dbc157ceed197489bc964ec90ee774b0e
Marco Fugaro <marco.fugaro@gmail.com>
2021-04-27 22:06:41 +0200

    Convert to ES6

--- src/Stats.js
+++ src/Stats.js
@@ -1,171 +1,106 @@
+import Panel from './Panel.js'
+
 /**
  * @author mrdoob / http://mrdoob.com/
  */
 
-var Stats = function () {
+class Stats {
 
-	var mode = 0;
+	constructor() {
+		this.mode = 0;
+		this.beginTime = performance.now();
+		this.prevTime = this.beginTime;
+		this.frames = 0;
 
-	var container = document.createElement( 'div' );
-	container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
-	container.addEventListener( 'click', function ( event ) {
+		this.REVISION = 16;
 
-		event.preventDefault();
-		showPanel( ++ mode % container.children.length );
+		const container = document.createElement( 'div' );
+		container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
+		container.addEventListener( 'click', ( event ) => {
 
-	}, false );
+			event.preventDefault();
+			this.showPanel( ++ this.mode % container.children.length );
 
-	//
+		} );
+		this.dom = container;
 
-	function addPanel( panel ) {
+		this.fpsPanel = this.addPanel( new Panel( 'FPS', '#0ff', '#002' ) );
+		this.msPanel = this.addPanel( new Panel( 'MS', '#0f0', '#020' ) );
 
-		container.appendChild( panel.dom );
+		if ( performance && performance.memory ) {
+
+			this.memPanel = this.addPanel( new Panel( 'MB', '#f08', '#201' ) );
+
+		}
+
+
+		this.showPanel( 0 );
+
+	}
+
+
+	 addPanel( panel ) {
+
+		this.dom.appendChild( panel.dom );
 		return panel;
 
 	}
 
-	function showPanel( id ) {
+	 showPanel( id ) {
 
-		for ( var i = 0; i < container.children.length; i ++ ) {
+		for ( var i = 0; i < this.dom.children.length; i ++ ) {
 
-			container.children[ i ].style.display = i === id ? 'block' : 'none';
+			this.dom.children[ i ].style.display = i === id ? 'block' : 'none';
 
 		}
 
-		mode = id;
+		this.mode = id;
 
 	}
 
-	//
 
-	var beginTime = ( performance || Date ).now(), prevTime = beginTime, frames = 0;
+	begin() {
 
-	var fpsPanel = addPanel( new Stats.Panel( 'FPS', '#0ff', '#002' ) );
-	var msPanel = addPanel( new Stats.Panel( 'MS', '#0f0', '#020' ) );
-
-	if ( self.performance && self.performance.memory ) {
-
-		var memPanel = addPanel( new Stats.Panel( 'MB', '#f08', '#201' ) );
+		this.beginTime = performance.now();
 
 	}
 
-	showPanel( 0 );
+	end() {
 
-	return {
+		this.frames ++;
 
-		REVISION: 16,
+		const time = performance.now();
 
-		dom: container,
+		this.msPanel.update( time - this.beginTime, 200 );
 
-		addPanel: addPanel,
-		showPanel: showPanel,
+		if ( time >= this.prevTime + 1000 ) {
 
-		begin: function () {
+			this.fpsPanel.update( ( this.frames * 1000 ) / ( time - this.prevTime ), 100 );
 
-			beginTime = ( performance || Date ).now();
+			this.prevTime = time;
+			this.frames = 0;
 
-		},
+			if ( this.memPanel ) {
 
-		end: function () {
-
-			frames ++;
-
-			var time = ( performance || Date ).now();
-
-			msPanel.update( time - beginTime, 200 );
-
-			if ( time >= prevTime + 1000 ) {
-
-				fpsPanel.update( ( frames * 1000 ) / ( time - prevTime ), 100 );
-
-				prevTime = time;
-				frames = 0;
-
-				if ( memPanel ) {
-
-					var memory = performance.memory;
-					memPanel.update( memory.usedJSHeapSize / 1048576, memory.jsHeapSizeLimit / 1048576 );
-
-				}
+				const memory = performance.memory;
+				this.memPanel.update( memory.usedJSHeapSize / 1048576, memory.jsHeapSizeLimit / 1048576 );
 
 			}
 
-			return time;
-
-		},
-
-		update: function () {
-
-			beginTime = this.end();
-
-		},
-
-		// Backwards Compatibility
-
-		domElement: container,
-		setMode: showPanel
-
-	};
-
-};
-
-Stats.Panel = function ( name, fg, bg ) {
-
-	var min = Infinity, max = 0, round = Math.round;
-	var PR = round( window.devicePixelRatio || 1 );
-
-	var WIDTH = 80 * PR, HEIGHT = 48 * PR,
-			TEXT_X = 3 * PR, TEXT_Y = 2 * PR,
-			GRAPH_X = 3 * PR, GRAPH_Y = 15 * PR,
-			GRAPH_WIDTH = 74 * PR, GRAPH_HEIGHT = 30 * PR;
-
-	var canvas = document.createElement( 'canvas' );
-	canvas.width = WIDTH;
-	canvas.height = HEIGHT;
-	canvas.style.cssText = 'width:80px;height:48px';
-
-	var context = canvas.getContext( '2d' );
-	context.font = 'bold ' + ( 9 * PR ) + 'px Helvetica,Arial,sans-serif';
-	context.textBaseline = 'top';
-
-	context.fillStyle = bg;
-	context.fillRect( 0, 0, WIDTH, HEIGHT );
-
-	context.fillStyle = fg;
-	context.fillText( name, TEXT_X, TEXT_Y );
-	context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
-
-	context.fillStyle = bg;
-	context.globalAlpha = 0.9;
-	context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
-
-	return {
-
-		dom: canvas,
-
-		update: function ( value, maxValue ) {
-
-			min = Math.min( min, value );
-			max = Math.max( max, value );
-
-			context.fillStyle = bg;
-			context.globalAlpha = 1;
-			context.fillRect( 0, 0, WIDTH, GRAPH_Y );
-			context.fillStyle = fg;
-			context.fillText( round( value ) + ' ' + name + ' (' + round( min ) + '-' + round( max ) + ')', TEXT_X, TEXT_Y );
-
-			context.drawImage( canvas, GRAPH_X + PR, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT, GRAPH_X, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT );
-
-			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, GRAPH_HEIGHT );
-
-			context.fillStyle = bg;
-			context.globalAlpha = 0.9;
-			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round( ( 1 - ( value / maxValue ) ) * GRAPH_HEIGHT ) );
-
 		}
 
-	};
+		return time;
+
+	}
+
+	update() {
+
+		this.beginTime = this.end();
+
+	}
 
 };
 
-export { Stats as default };
+
+export default Stats;
+export { Panel };
